84780e15c684d4aaa734be9ca8ba2f269ccf3195
sbm367 <sbm367@protonmail.com>
2021-03-09 16:42:33 -0500

    added spaing vars

--- arithmetic.py
+++ arithmetic.py
@@ -7,9 +7,12 @@ def arithmetic_arranger(al, solved=False):
     express = {
         'top':[],
         'bottom':[],
-        'operator':[]
+        'operator':[],
+        'width':[]
     }
     longest = 0
+    units = len(al)
+    
     for expresion in al:
         if '-' in expresion or '+' in expresion:
             sub = expresion.split(' ')
@@ -26,11 +29,13 @@ def arithmetic_arranger(al, solved=False):
             else:
                  if lb > longest:
                     longest = lb
-
+            express['width'].append(longest+2)
             print(sub)
         else:
             return('Error, not an arithmetic expresion')
     print(express)
     print(longest)
+    white_spacing = 4 
+
 
 arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)
